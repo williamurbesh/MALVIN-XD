92d8c3c6aa9095aacb7eb1b82f1ebc5452672935
williamurbesh <goldenauberone@gmail.com>
2025-09-07 10:25:58 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~42Ewgb6Z#qNBUuS5_iLussfmVAdNStLIZK75YcOG15k98Ugl-oIo",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
